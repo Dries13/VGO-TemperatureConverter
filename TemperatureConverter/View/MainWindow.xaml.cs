bee3169435c87e271aeab793c0c55c9c85218f2c
Dries_Roosen <91371319+Dries13@users.noreply.github.com>
2023-04-06 20:35:22 +0200

    kelvin

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -36,6 +37,33 @@ namespace View
         {
             var cel = double.Parse(textboxCel.Text);
             var far = Math.Round((1.8*cel + 32), 2);
+            textboxFar.Text = far.ToString();
+        }
+
+        private void ConvertFahrenheit(object sender, RoutedEventArgs e)
+        {
+            var far = double.Parse(textboxFar.Text);
+            var cel = Math.Round((far - 32) / 1.8, 2);
+            textboxCel.Text = cel.ToString();
+            var kel = cel+273.15;
+            textboxKel.Text = kel.ToString();
+        }
+
+        private void ConvertCelsius(object sender, RoutedEventArgs e)
+        {
+            var cel = double.Parse(textboxCel.Text);
+            var far = Math.Round((1.8 * cel + 32), 2);
+            textboxFar.Text = far.ToString();
+            var kel = cel+273.15;
+            textboxKel.Text = kel.ToString();
+        }
+
+        private void ConvertKelvin(object sender, RoutedEventArgs e)
+        {
+            var kel = double.Parse(textboxKel.Text);
+            var cel = kel - 273.15;
+            textboxCel.Text = cel.ToString();
+            var far = Math.Round((1.8 * cel + 32), 2);
             textboxFar.Text = far.ToString();
         }
     }
